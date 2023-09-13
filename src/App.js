773a832b47d8d4806fbc5cf2ab1686f13b23217a
Michal Kurek (Mike) <michal@kurek.pl>
2023-09-13 11:38:22 +0200

    add 'handleChange' method and 'name' field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  state = {
+    username: "",
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleChange = e => {
+    console.log(e.target.type);
+    console.log(e.target.name);
+
+    const value = e.target.value;
+    this.setState({
+      username: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="user">
+            Name:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
